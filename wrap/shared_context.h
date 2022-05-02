015350eb92a8ef1182fe65a060c71f03005d55ed
Alexandre Paillier <alexandre.paillier@ledger.fr>
2022-05-02 17:11:28 +0200

    Now wraps the printf calls in a macro. SDK-like

--- wrap/shared_context.h
+++ wrap/shared_context.h
@@ -1,9 +1,13 @@
 #ifndef SHARED_CONTEXT_H_
 #define SHARED_CONTEXT_H_
 
+#include <stdio.h>
+
 #include "libcxng.h"
 
 #define ADDRESS_LENGTH 20
+
+#define PRINTF(fmt, ...) printf(fmt, ##__VA_ARGS__)
 
 extern cx_sha3_t    global_sha3;
 
